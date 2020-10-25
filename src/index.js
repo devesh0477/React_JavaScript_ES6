2cc214ef52ec5bb842e53ba46777b89b328d4e66
devesh0477 <55842034+devesh0477@users.noreply.github.com>
2020-10-25 02:00:08 -0400

    done with 2 methods

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// -----------------------------------------------------------------------------------
+//                          METHOD: 1
+// -----------------------------------------------------------------------------------
+
+// import abc, { doublePi, triplePi } from "./math.js";
+
+// ReactDOM.render(
+//   <ul>
+//     <li>{abc}</li>
+//     <li>{doublePi()}</li>
+//     <li>{triplePi()}</li>
+//   </ul>,
+//   document.getElementById("root")
+// );
+
+// -----------------------------------------------------------------------------------
+//                          METHOD:2
+// -----------------------------------------------------------------------------------
+
+import * as abc from "./math.js";
+
 ReactDOM.render(
   <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
+    <li>{abc.default}</li>
+    <li>{abc.doublePi()}</li>
+    <li>{abc.triplePi()}</li>
   </ul>,
   document.getElementById("root")
 );
